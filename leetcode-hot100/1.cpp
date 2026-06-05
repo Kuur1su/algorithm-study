b6828ed24b2ab32c5a954f90ddb2acec938a80fa
Kuur1su <3325771476@qq.com>
2026-06-05 15:21:13 +0800

    2025/6/5

--- leetcode-hot100/1.cpp
+++ leetcode-hot100/1.cpp
@@ -10,8 +10,11 @@ using i128 = __int128;
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
- 
-    
-    
+
+    int a, b;
+    std::cin >> a >> b;
+
+    std::cout << a + b << "\n";
+
     return 0;
 }
